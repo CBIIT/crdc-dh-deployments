d929735bbf1023920f44ebc40f7d168bd3c3d5f6
Tracy Truong <tracy18@users.noreply.github.com>
2023-05-19 17:45:28 -0400

    adding new tf modules

---
+++ terraform/monitoring.tf
@@ -0,0 +1,8 @@
+module "monitoring" {
+  source               = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/monitoring?ref=v1.4"
+  app                  = var.project
+  tags                 = var.tags
+  sumologic_access_id  = var.sumologic_access_id
+  sumologic_access_key = var.sumologic_access_key
+  microservices        = var.microservices
+}
