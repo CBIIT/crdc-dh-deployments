ed6617bf99fe9a3129c633c23299dcdccaf92276
Tracy Truong <tracy18@users.noreply.github.com>
2023-05-24 17:17:47 -0400

    try without s3

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -12,15 +12,15 @@ module "alb" {
   alb_certificate_arn = data.aws_acm_certificate.amazon_issued.arn
 }
 
-module "s3" {
-  source                        = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/s3?ref=v1.4"
-  bucket_name                   = local.alb_log_bucket_name
-  stack_name                    = var.project
-  env                           = terraform.workspace
-  tags                          = var.tags
-  s3_force_destroy              = var.s3_force_destroy
-  days_for_archive_tiering      = 125
-  days_for_deep_archive_tiering = 180
-  s3_enable_access_logging      = false
-  s3_access_log_bucket_id       = ""
-}
+#module "s3" {
+#  source                        = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/s3?ref=v1.4"
+#  bucket_name                   = local.alb_log_bucket_name
+#  stack_name                    = var.project
+#  env                           = terraform.workspace
+#  tags                          = var.tags
+#  s3_force_destroy              = var.s3_force_destroy
+#  days_for_archive_tiering      = 125
+#  days_for_deep_archive_tiering = 180
+#  s3_enable_access_logging      = false
+#  s3_access_log_bucket_id       = ""
+#}
